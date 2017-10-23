f68c96be36eeb19a10efb1240ac971d6a141c40a
JSugey <jesyk.suu@gmail.com>
2017-10-23 11:18:52 -0500

    Creación del proyecto Pago en linea

---
+++ src/main/webapp/app/app.module.ts
@@ -0,0 +1,57 @@
+import './vendor.ts';
+
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Ng2Webstorage } from 'ng2-webstorage';
+
+import { PagolineaSharedModule, UserRouteAccessService } from './shared';
+import { PagolineaHomeModule } from './home/home.module';
+import { PagolineaAdminModule } from './admin/admin.module';
+import { PagolineaAccountModule } from './account/account.module';
+import { PagolineaEntityModule } from './entities/entity.module';
+
+import { customHttpProvider } from './blocks/interceptor/http.provider';
+import { PaginationConfig } from './blocks/config/uib-pagination.config';
+
+// jhipster-needle-angular-add-module-import JHipster will add new module here
+
+import {
+    JhiMainComponent,
+    LayoutRoutingModule,
+    NavbarComponent,
+    FooterComponent,
+    ProfileService,
+    PageRibbonComponent,
+    ActiveMenuDirective,
+    ErrorComponent
+} from './layouts';
+
+@NgModule({
+    imports: [
+        BrowserModule,
+        LayoutRoutingModule,
+        Ng2Webstorage.forRoot({ prefix: 'jhi', separator: '-'}),
+        PagolineaSharedModule,
+        PagolineaHomeModule,
+        PagolineaAdminModule,
+        PagolineaAccountModule,
+        PagolineaEntityModule,
+        // jhipster-needle-angular-add-module JHipster will add new module here
+    ],
+    declarations: [
+        JhiMainComponent,
+        NavbarComponent,
+        ErrorComponent,
+        PageRibbonComponent,
+        ActiveMenuDirective,
+        FooterComponent
+    ],
+    providers: [
+        ProfileService,
+        customHttpProvider(),
+        PaginationConfig,
+        UserRouteAccessService
+    ],
+    bootstrap: [ JhiMainComponent ]
+})
+export class PagolineaAppModule {}
